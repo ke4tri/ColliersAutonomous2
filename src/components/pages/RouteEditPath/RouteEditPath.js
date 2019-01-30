35cae53543aab969b2979da693816e3bd4630672
Wayne Collier <ke4tri@gmail.com>
2019-01-29 19:30:28 -0600

    direction buttons ordered

--- src/components/pages/RouteEditPath/RouteEditPath.js
+++ src/components/pages/RouteEditPath/RouteEditPath.js
@@ -11,8 +11,25 @@ class RouteEditPath extends React.Component {
     return (
       <div className='Home mx-auto'>
         <h2>RouteEditPath</h2>
-        <button class="btn btn-primary" onClick={this.changeView2}>Button</button>
-      </div>
+        <div clasName="wrapButton d-flex justify-content-center">
+          {/* <button class="btn btn-primary" onClick={this.changeView2}>Button</button> */}
+          <div className="btn-group d-flex justify-content-center">
+            <button type="button" className="taco btn btn-outline-light m-2 p-3">TAKE OFF</button>
+            <button type="button" className="taco btn btn-outline-light m-2 p-3">UP</button>
+            <button type="button" className="taco btn btn-outline-light m-2 p-3">LAND</button>
+          </div>
+          <div class="btn-group d-flex justify-content-center">
+            <button type="button" className="btn btn-outline-light m-2 p-3">90CW</button>
+            <button type="button" className="btn btn-outline-light m-2 p-3">PHOTO</button>
+            <button type="button" className="btn btn-outline-light m-2 p-3">90CC</button>
+          </div>
+          <div class="btn-group d-flex justify-content-center">
+            <button type="button" className="taco2 btn btn-outline-light m-2 p-3">FORWARD</button>
+            <button type="button" className="taco2 btn btn-outline-light m-2 p-3">DOWN</button>
+            <button type="button" className="taco2 btn btn-outline-light m-2 p-3">BACK</button>
+          </div>
+        </div>
+     </div>
     );
   }
 }
