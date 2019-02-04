c590aad38e8846528e19ad50bad1ec12e8ae4e27
Wayne Collier <ke4tri@gmail.com>
2019-02-03 19:49:05 -0600

    MVP

--- src/components/pages/LaunchDisplay/LaunchDisplay.js
+++ src/components/pages/LaunchDisplay/LaunchDisplay.js
@@ -16,13 +16,19 @@ class LaunchDisplay extends React.Component {
     // this.device();
   }
 
-  render() {
-    const {
-      currentDevice,
-      currentLocation,
-      currentRoute,
-    } = this.props;
-    return (
+launchThis = () => {
+  const launchCmd = this.props.currentRoute.cmd;
+  console.log(launchCmd);
+}
+
+render() {
+  const {
+    currentDevice,
+    currentLocation,
+    currentRoute,
+  } = this.props;
+
+  return (
       <div className="wrapperNumOne">
         <div className='Home mx-auto'>
           <h2>LaunchDisplay</h2>
@@ -31,11 +37,11 @@ class LaunchDisplay extends React.Component {
           <div>{currentRoute.cmd}</div>
         </div>
         <div>
-          <button className="btn btn-outline-success">LAUNCH</button>
+          <button className="btn btn-outline-success" onClick={this.launchThis}>LAUNCH</button>
         </div>
       </div>
-    );
-  }
+  );
+}
 }
 
 export default LaunchDisplay;
