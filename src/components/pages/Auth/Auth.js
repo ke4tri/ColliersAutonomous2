807f658e124dc701fc4c6c53af7dc458fed91ece
Wayne Collier <ke4tri@gmail.com>
2019-02-22 10:10:44 -0600

    mobile css updated

--- src/components/pages/Auth/Auth.js
+++ src/components/pages/Auth/Auth.js
@@ -17,6 +17,7 @@ class Auth extends React.Component {
             <img className="hero-logo-circle" src="https://github.com/ke4tri/personal-bi-site2/blob/master/src/img/prop2.png?raw=true" alt="Index portal blue"/>
             <img className="hero-logo-circle slogan" src="https://github.com/ke4tri/Images/blob/master/slogan_use.png?raw=true" alt="Index portal blue"/>
             <h2 className="hero-logo-circle slogan2">"Taking Drones to a Higher Level"</h2>
+            <h2 className="slogan2">Login Above</h2>
         </div>
       </div>
     );
