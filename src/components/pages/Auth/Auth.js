ebe4c7bfa6bdd207a1d015049517e5856e241f89
Wayne Collier <ke4tri@gmail.com>
2019-02-22 09:36:15 -0600

    both expaned and mobile are hidding/showing element

--- src/components/pages/Auth/Auth.js
+++ src/components/pages/Auth/Auth.js
@@ -16,8 +16,7 @@ class Auth extends React.Component {
             <img className="hero-logo-circle" src="https://github.com/ke4tri/personal-bi-site2/blob/master/src/img/prop1.png?raw=true" alt="Index portal blue"/>
             <img className="hero-logo-circle" src="https://github.com/ke4tri/personal-bi-site2/blob/master/src/img/prop2.png?raw=true" alt="Index portal blue"/>
             <img className="hero-logo-circle slogan" src="https://github.com/ke4tri/Images/blob/master/slogan_use.png?raw=true" alt="Index portal blue"/>
-            {/* <img className="hero-logo-circle slogan2" src="https://github.com/ke4tri/Images/blob/master/slogan_use.png?raw=true" alt="Index portal"/> */}
-            <h2 className="hero-logo-circle">"Taking Drones to a Higher Level"</h2>
+            <h2 className="hero-logo-circle slogan2">"Taking Drones to a Higher Level"</h2>
         </div>
       </div>
     );
